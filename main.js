22875931fa4ca33dd3fcb91e2772d8896ea7f6d9
Harvey Gomez <hmgomez90@gmail.com>
2018-08-24 15:20:33 +0100

    Basic tests for the code and completed the Mongo closing of connection

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const cheerio = require('cheerio');
 const mongodb = require('mongodb');
 
 const baseUrl = 'https://coinmarketcap.com/exchanges/volume/24-hour/';
-const volumesArray = [];
+const exchanges = [ 'binance','bitfinex','okex','huobi','bittrex', 'poloniex', 'cryptopia', 'bittrex','bitstamp', 'kraken', 'coinbase-pro', 'bithumb', 'simex', 'digifinex', 'zb-com', 'bibox', 'bit-z']
+var volumesArray = [];
+
 
 /**
  * Handles the inserting of an array into the mongoDB database.
@@ -20,19 +22,34 @@ function AddVolumesToDatabase(array) {
             console.log('Databse connection established.');
             var db = client.db('exchanges');
             var collection = db.collection('tradeVolumes');           
-            array.forEach(function(item) {
+            array.forEach(function(item, index) {
                 collection.insert(item, function(error, inserted) {
                     if (error) {
-                        console.log(error);
-                    } else {
-                        console.log(inserted);
+                        console.log(`Database insert FAILED: ${error}`);
+                    } else if (index + 1 == array.length) {
+                        console.log("Database insert completed.")
+                        client.close();
                     }
                 });
             });
-
-            return;
         }
     });
+}
+
+/**
+ * Filters the JSON data response from the crawled site and builds a new array for only the prices
+ * associated with the exchanges in the defined array called exchanges. 
+ * @param {object} array 
+ */
+function filterJsonResponse(array) {
+    let dispoableArray = [];
+    array.forEach(function(item, index) {
+        if (exchanges.includes(item.name)) {
+            dispoableArray.push(item);
+        }
+    });
+    
+    return dispoableArray;
 }
 
 /**
@@ -49,7 +66,7 @@ function sanitiseStringToNumber(string) {
 }
 
 /**
- * Request to get all exchange values from the target website. Builds an arra with the results
+ * Request to get all exchange values from the target website. Builds an array with the results
  * and passes to the database handler method. 
  */
 function crawlSite() {
@@ -57,16 +74,16 @@ function crawlSite() {
         if (!error) {
             const $ = cheerio.load(body);
             const tableRows = $('.table-condensed tbody tr')
+            const localArray = [];
             var exchangeName;
     
             tableRows.each(function(index, element) {
                 if (element.attribs.id) {
-                    var aa = $(element).nextUntil('tr').attr('id');
                     var correctElement = tableRows[index - 1];
                     if (correctElement) {
                         var volumeValue = $(correctElement).find('.volume').text();
                         volumeValue = sanitiseStringToNumber(volumeValue);
-                        volumesArray.push({ 
+                        localArray.push({ 
                             'name': exchangeName,
                             'volume': volumeValue,
                             'timestamp': Date.now() 
@@ -77,8 +94,11 @@ function crawlSite() {
                     exchangeName = element.attribs.id;
                 }
             });
-    
+            console.log('JSON obtained from website');
+            volumesArray = filterJsonResponse(localArray);
+            console.log('Array prepared for insert, trying now...');
             AddVolumesToDatabase(volumesArray);
+            console.log(" --- Job completed ---");
     
         } else {
             console.log(`Unable to complete the request: ${error}`);
@@ -87,8 +107,3 @@ function crawlSite() {
 }
 
 crawlSite();
-
-// module.exports = {
-//     sanitiseStringToNumber: sanitiseStringToNumber,
-//     AddVolumesToDatabase: AddVolumesToDatabase
-// }
--- test/mainTest.js
+++ test/mainTest.js
@@ -2,11 +2,18 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 
 const sanitiseStringToNumber = require('../core').sanitiseStringToNumber;
-//const sanitiseStringToNumber = require('../main').sanitiseStringToNumber;
+
+const sanitiseStringToNumber = require('../main').sanitiseStringToNumber;
 
 describe('Sanitise A String To Number', function() {
     it('Should return a type of number', function() {
         let result = sanitiseStringToNumber('1234');
         assert.typeOf(result, 'number');
+    });
+});
+
+describe('Add Volumes To Database', function() {
+    it('Should connect to the database and add to its volumes', function() {
+        assert.equal();
     });
 });
